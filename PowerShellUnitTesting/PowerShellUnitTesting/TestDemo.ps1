05d8f179de41f3a690c849674eb8f2af959ceff3
Francois Delport <fran.cois@hotmail.com>
2016-05-14 22:17:58 +1000

    Initial Commit

---
+++ PowerShellUnitTesting/PowerShellUnitTesting/TestDemo.ps1
@@ -0,0 +1,28 @@
+﻿function Myfunction($myparam)
+{
+	$myparam | Write-Output
+}
+
+function GetMyOwnDate()
+{ 
+	return Get-Date
+}
+
+function DoStuffToFolders($folder)
+{
+	Myfunction $folder
+
+	if (Get-Item $folder)
+	{
+		Remove-Item -Path $folder
+		return "Deleted Folder"
+	}
+	else
+	{
+		New-Item -ItemType Directory -Name $folder
+		return "Created folder"
+	}
+}
+
+
+
---
+++ PowerShellUnitTesting/PowerShellUnitTesting/TestDemo.tests.ps1
@@ -0,0 +1,46 @@
+﻿$project = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace(".Tests", "")
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".tests.", ".")
+. "$project\$sut"
+
+Describe "ManipulateFolders"{
+
+	#Scoped to all tests under Describe
+	Mock Get-Date { New-Object DateTime (2000, 1, 1) }
+
+	Context "Depending on the existence of a folder" {
+		$folder = "c:\temp"
+		#Mock my function with the expected parameter
+		Mock Myfunction {} -Verifiable -ParameterFilter {$myparam -eq $folder}
+		#Mock these functions to manipulate the file system and scope to Context
+		Mock Remove-Item
+		Mock New-Item
+
+		It "It Should Delete The Folder" {
+			Mock Get-Item { $true }   #return true, scoped to It
+			$result = DoStuffToFolders $folder
+			Assert-VerifiableMocks #will fail if any -Verifiable mocks didn't meet criteria
+			$result | Should be "Deleted folder"
+		}
+		It "It Should Create The Folder" {
+			Mock Get-Item { $false }   #return false, scoped to It
+			$result = DoStuffToFolders $folder
+			Assert-VerifiableMocks #will fail if any -Verifiable mocks didn't meet criteria
+			$result | Should be "Created folder"
+		}
+	}
+
+	Context "When you get a date"{
+			$date = GetMyOwnDate
+
+			It "It Should Be The Year 2000"{
+				($date).year | Should be "2000"
+			}
+		}
+}
+
+
+
+
+
+
+
